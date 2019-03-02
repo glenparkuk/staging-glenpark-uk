055ad7313e895485a7f45b90d4ce0ffef4aec682
Chris Park-Kennaby <parkkennaby@google.com>
2019-03-02 23:58:28 +0000

    add scroll js to scripts.js, remove js from html

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -552,3 +552,28 @@ jQuery.cookie = function(name, value, options) {
 		return cookieValue;
 	}
 };
+$(document).ready(function(){
+	$(".scroll").click(function(event){
+		//prevent the default action for the click event
+		event.preventDefault();
+		//get the full url - like mysitecom/index.htm#home
+		var full_url = this.href;
+		//split the url by # and get the anchor target name - home in mysitecom/index.htm#home
+		var parts = full_url.split("#");
+		var trgt = parts[1];
+		//get the top offset of the target anchor
+		var target_offset = $("#"+trgt).offset();
+		var target_top = target_offset.top;
+		//goto that anchor by setting the body scroll top to anchor top
+		$('html, body').animate({scrollTop:target_top}, 500);
+	});
+  $(".scroll-buy-now").click(function(event){
+    //prevent the default action for the click event
+    event.preventDefault();
+    //get the top offset of the target anchor
+    var target_offset = $("#buyNow").offset();
+    var target_top = target_offset.top;
+    //goto that anchor by setting the body scroll top to anchor top
+    $('html, body').animate({scrollTop:target_top}, 1000);
+  });
+});

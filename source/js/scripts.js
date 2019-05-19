8aaac8b472bf7c8e50070391e38b3b97c30184b0
chrispk <chrispkennaby@gmail.com>
2019-05-19 21:00:18 +0100

    move all js sources into all.min.js, update head

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -104,17 +104,7 @@ function initialisePaypalExpressCheckout() {
 }
 
 
-(function($) { // reference query
-	$(function() { // shorthand for onLoad()
 
-
-		$(".js-slick-carousel").slick({
-			fade: true,
-			autoplay: true
-		});
-
-	});
-})(jQuery);
 
 
 
@@ -267,8 +257,61 @@ function hideShowSubmenu() {
 
 }
 
-(function($){
-	$.fn.extend({
+
+
+
+
+
+/* COOKIES PLUGIN */
+
+jQuery.cookie = function(name, value, options) {
+	if (typeof value != 'undefined') { // name and value given, set cookie
+		options = options || {};
+		if (value === null) {
+			value = '';
+			options.expires = -1;
+		}
+		var expires = '';
+		if (options.expires && (typeof options.expires == 'number' || options.expires.toUTCString)) {
+			var date;
+			if (typeof options.expires == 'number') {
+				date = new Date();
+				date.setTime(date.getTime() + (options.expires * 24 * 60 * 60 * 1000));
+			} else {
+				date = options.expires;
+			}
+			expires = '; expires=' + date.toUTCString(); // use expires attribute, max-age is not supported by IE
+		}
+		// CAUTION: Needed to parenthesize options.path and options.domain
+		// in the following expressions, otherwise they evaluate to undefined
+		// in the packed version for some reason...
+		var path = options.path ? '; path=' + (options.path) : '';
+		var domain = options.domain ? '; domain=' + (options.domain) : '';
+		var secure = options.secure ? '; secure' : '';
+		document.cookie = [name, '=', encodeURIComponent(value), expires, path, domain, secure].join('');
+	} else { // only name given, get cookie
+		var cookieValue = null;
+		if (document.cookie && document.cookie != '') {
+			var cookies = document.cookie.split(';');
+			for (var i = 0; i < cookies.length; i++) {
+				var cookie = jQuery.trim(cookies[i]);
+				// Does this cookie string begin with the name we want?
+				if (cookie.substring(0, name.length + 1) == (name + '=')) {
+					cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+					break;
+				}
+			}
+		}
+		return cookieValue;
+	}
+};
+
+
+
+(function($) { // reference query
+	$(function() { // shorthand for onLoad()
+
+		$.fn.extend({
 
 		portfolioSlide: function() {
 
@@ -460,76 +503,37 @@ function hideShowSubmenu() {
 
 	});
 
-})(jQuery);
+		$(".scroll").click(function(event){
+				//prevent the default action for the click event
+				event.preventDefault();
+				//get the full url - like mysitecom/index.htm#home
+				var full_url = this.href;
+				//split the url by # and get the anchor target name - home in mysitecom/index.htm#home
+				var parts = full_url.split("#");
+				var trgt = parts[1];
+				//get the top offset of the target anchor
+				var target_offset = $("#"+trgt).offset();
+				var target_top = target_offset.top;
+				//goto that anchor by setting the body scroll top to anchor top
+				$('html, body').animate({scrollTop:target_top}, 500);
+			});
+		  $(".scroll-buy-now").click(function(event){
+		    //prevent the default action for the click event
+		    event.preventDefault();
+		    //get the top offset of the target anchor
+		    var target_offset = $("#buyNow").offset();
+		    var target_top = target_offset.top;
+		    //goto that anchor by setting the body scroll top to anchor top
+		    $('html, body').animate({scrollTop:target_top}, 1000);
+		  });
 
 
+		$(".js-slick-carousel").slick({
+			fade: true,
+			autoplay: true
+		});
 
+		menuHovers();
 
-/* COOKIES PLUGIN */
-
-jQuery.cookie = function(name, value, options) {
-	if (typeof value != 'undefined') { // name and value given, set cookie
-		options = options || {};
-		if (value === null) {
-			value = '';
-			options.expires = -1;
-		}
-		var expires = '';
-		if (options.expires && (typeof options.expires == 'number' || options.expires.toUTCString)) {
-			var date;
-			if (typeof options.expires == 'number') {
-				date = new Date();
-				date.setTime(date.getTime() + (options.expires * 24 * 60 * 60 * 1000));
-			} else {
-				date = options.expires;
-			}
-			expires = '; expires=' + date.toUTCString(); // use expires attribute, max-age is not supported by IE
-		}
-		// CAUTION: Needed to parenthesize options.path and options.domain
-		// in the following expressions, otherwise they evaluate to undefined
-		// in the packed version for some reason...
-		var path = options.path ? '; path=' + (options.path) : '';
-		var domain = options.domain ? '; domain=' + (options.domain) : '';
-		var secure = options.secure ? '; secure' : '';
-		document.cookie = [name, '=', encodeURIComponent(value), expires, path, domain, secure].join('');
-	} else { // only name given, get cookie
-		var cookieValue = null;
-		if (document.cookie && document.cookie != '') {
-			var cookies = document.cookie.split(';');
-			for (var i = 0; i < cookies.length; i++) {
-				var cookie = jQuery.trim(cookies[i]);
-				// Does this cookie string begin with the name we want?
-				if (cookie.substring(0, name.length + 1) == (name + '=')) {
-					cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-					break;
-				}
-			}
-		}
-		return cookieValue;
-	}
-};
-$(document).ready(function(){
-	$(".scroll").click(function(event){
-		//prevent the default action for the click event
-		event.preventDefault();
-		//get the full url - like mysitecom/index.htm#home
-		var full_url = this.href;
-		//split the url by # and get the anchor target name - home in mysitecom/index.htm#home
-		var parts = full_url.split("#");
-		var trgt = parts[1];
-		//get the top offset of the target anchor
-		var target_offset = $("#"+trgt).offset();
-		var target_top = target_offset.top;
-		//goto that anchor by setting the body scroll top to anchor top
-		$('html, body').animate({scrollTop:target_top}, 500);
 	});
-  $(".scroll-buy-now").click(function(event){
-    //prevent the default action for the click event
-    event.preventDefault();
-    //get the top offset of the target anchor
-    var target_offset = $("#buyNow").offset();
-    var target_top = target_offset.top;
-    //goto that anchor by setting the body scroll top to anchor top
-    $('html, body').animate({scrollTop:target_top}, 1000);
-  });
-});
+})(jQuery);

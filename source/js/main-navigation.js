3d4b97d71bdcf129188b2a4fb9d775ed26eb76a3
Joe <jparkkennaby@gmail.com>
2020-06-06 15:00:03 +0100

    setup mneu hover and submenu display logic using simpler scss and jqoery functionality

--- source/js/main-navigation.js
+++ source/js/main-navigation.js
@@ -2,8 +2,32 @@
     $(function () { // shorthand for onLoad()
 
 
+        var $menuItems = $('.js-menu-item');
+        // var $subMenu = $('.js-sub-menu-item');
 
+        $menuItems.each(function(key, menuItem) {
 
+            var $menuItem = $(menuItem);
+
+            if ($menuItem.length === 0) return;
+
+            $menuItem.on('mouseenter', function() {
+
+                $subMenuItem = $menuItem.find('.js-sub-menu-item');
+
+                $subMenuItem.addClass('is-active');
+
+            });
+
+            $menuItem.on('mouseleave', function() {
+
+                $subMenuItem = $menuItem.find('.js-sub-menu-item');
+
+                $subMenuItem.removeClass('is-active');
+
+            });
+
+        });
 
 
         // TODO DELETE THE BELOW CODE
@@ -156,7 +180,7 @@
 
         }
 
-        menuHovers();
+        // menuHovers();
 
     });
 })(jQuery);
